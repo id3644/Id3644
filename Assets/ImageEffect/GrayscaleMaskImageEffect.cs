5aa47b080c60f0182be181580cd2944c8dcc8b66
park je hun <id3644@gmail.com>
2017-07-02 23:36:20 +0900

    aaa

---
+++ Assets/ImageEffect/GrayscaleMaskImageEffect.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrayscaleMaskImageEffect : MonoBehaviour
+{
+    public Material Material;
+
+    void OnEnable()
+    {
+        if(Material == null)
+        {
+            Debug.LogError("Material is not assigned.", this);
+            this.enabled = false;
+        }
+    }
+
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        Graphics.Blit(src, dest, Material);
+    }
+}
